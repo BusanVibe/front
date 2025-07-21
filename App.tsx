5731434d0f761d0357b8b7aa409d0f5c221ff0e9
sihyun <psh2968@naver.com>
2025-07-22 00:00:38 +0900

    feat: splash screen dev...

--- App.tsx
+++ App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/RootNavigator';
+import { View, Image, StyleSheet, Animated } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [showSplash, setShowSplash] = useState(true);
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start(() => setShowSplash(false));
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [fadeAnim]);
+
+  if (showSplash) {
+    return (
+      <Animated.View style={[styles.splashContainer, { opacity: fadeAnim }]}> 
+        <Image source={require('./src/assets/logo.png')} style={styles.logo} resizeMode="contain" />
+      </Animated.View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default App;
