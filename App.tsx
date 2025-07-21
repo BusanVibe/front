222e33ed64222b8ff0b7a849ded1b06ba3e4f5e0
Taeksin Kim <yoyo2521@naver.com>
2025-07-21 23:36:24 +0900

    feat: src폴더 이동 문제 해결

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
-import RootNavigator from './navigation/RootNavigator';
+import RootNavigator from './src/navigation/RootNavigator';
 
 function App(): React.JSX.Element {
   return (
