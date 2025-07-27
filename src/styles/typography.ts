0803b993dd8223be9f0b574274e2a99660a533c2
hyesngy <tell280210@gmail.com>
2025-07-27 13:32:05 +0900

    🎨 Design: 타이포 시스템 수정

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,4 +1,4 @@
-import { TextStyle } from 'react-native';
+import {TextStyle} from 'react-native';
 
 type Variant =
   | 'title'
@@ -15,59 +15,70 @@ type Variant =
 
 const typography: Record<Variant, TextStyle> = {
   title: {
+    fontFamily: 'Pretendard',
     fontSize: 20,
     fontWeight: '500',
     lineHeight: 20 * 1.2,
   },
   headingLg: {
+    fontFamily: 'Pretendard',
     fontSize: 18,
     fontWeight: '600',
     lineHeight: 18 * 1.2,
   },
   headingMd: {
+    fontFamily: 'Pretendard',
     fontSize: 16,
     fontWeight: '600',
     lineHeight: 16 * 1.2,
   },
   subHeadingLg: {
+    fontFamily: 'Pretendard',
     fontSize: 18,
     fontWeight: '500',
     lineHeight: 18 * 1.2,
   },
   subHeadingMd: {
+    fontFamily: 'Pretendard',
     fontSize: 16,
     fontWeight: '500',
     lineHeight: 16 * 1.2,
   },
   subHeadingSm: {
+    fontFamily: 'Pretendard',
     fontSize: 14,
     fontWeight: '500',
     lineHeight: 14 * 1.2,
   },
   bodyLg: {
+    fontFamily: 'Pretendard',
     fontSize: 14,
     fontWeight: '400',
-    lineHeight: 14 * 1.0,
+    lineHeight: 14 * 1.2,
   },
   bodyMd: {
+    fontFamily: 'Pretendard',
     fontSize: 12,
     fontWeight: '400',
-    lineHeight: 12 * 1.0,
+    lineHeight: 12 * 1.2,
   },
   bodySm: {
+    fontFamily: 'Pretendard',
     fontSize: 10,
     fontWeight: '400',
-    lineHeight: 10 * 1.0,
+    lineHeight: 10 * 1.2,
   },
   captionLg: {
+    fontFamily: 'Pretendard',
     fontSize: 12,
     fontWeight: '300',
-    lineHeight: 12 * 1.0,
+    lineHeight: 12 * 1.2,
   },
   captionMd: {
-    fontSize: 8,
+    fontFamily: 'Pretendard',
+    fontSize: 10,
     fontWeight: '300',
-    lineHeight: 8 * 1.0,
+    lineHeight: 10 * 1.2,
   },
 };
 
