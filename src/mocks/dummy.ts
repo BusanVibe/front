8c4380a032fafcb5b3015ea808621ef119eb2c05
hyesngy <tell280210@gmail.com>
2025-08-05 21:57:24 +0900

    ✨ Feat: 명소 더미데이터 추가

--- src/mocks/dummy.ts
+++ src/mocks/dummy.ts
@@ -22,42 +22,6 @@ export const curationData = [
   },
 ];
 
-// 명소 리스트 데이터
-export const attractionData: PlaceListItem[] = [
-  {
-    place_id: 1,
-    name: '광안리 해수욕장',
-    type: PlaceType.SIGHT,
-    congestion_level: 4,
-    is_like: false,
-    address: '부산 수영구 광안해변로',
-  },
-  {
-    place_id: 2,
-    name: '워킹홀리데이',
-    type: PlaceType.CAFE,
-    congestion_level: 3,
-    is_like: true,
-    address: '부산 수영구 광안해변로',
-  },
-  {
-    place_id: 3,
-    name: '광안리 해수욕장',
-    type: PlaceType.SIGHT,
-    congestion_level: 2,
-    is_like: false,
-    address: '부산 수영구 광안해변로',
-  },
-  {
-    place_id: 4,
-    name: '워킹홀리데이',
-    type: PlaceType.CAFE,
-    congestion_level: 1,
-    is_like: true,
-    address: '부산 수영구 광안해변로',
-  },
-];
-
 // 좋아요 목록 데이터 (정렬 기능 테스트용)
 export const favoriteData: PlaceListItem[] = [
   {
